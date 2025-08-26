0b83b78bf17ae03f7a2d885ecc4b8f29ababc0b3
AnaC-Araujo <carolinaana887@gmail.com>
2025-08-26 19:41:04 -0300

    Start commit

---
+++ exercicios/exer__01.py
@@ -0,0 +1,8 @@
+'''Exercício 1: Verificador de Positivo
+● Peça ao usuário para digitar um número inteiro.
+● Use uma estrutura if simples para verificar se o número é maior que zero.
+● Se for, imprima "O número é positivo!".'''
+
+numero = int(input("Digite um número: "))
+if numero > 0:
+    print("O seu número digitado é positivo!")
