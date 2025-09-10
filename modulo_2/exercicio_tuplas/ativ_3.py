fa86105bfa9260c91d8501f6c64125398afa063e
AnaC-Araujo <carolinaana887@gmail.com>
2025-09-10 15:25:14 -0300

    Contando Elementos

---
+++ modulo_2/exercicio_tuplas/ativ_3.py
@@ -0,0 +1,6 @@
+'''. Contando Elementos: Crie uma tupla numeros = (1, 2, 3, 2, 4, 2). Use o método .count()
+para descobrir quantas vezes o número 2 aparece.
+'''
+numeros = (1, 2, 3, 2, 4, 2, 5, 2, 6, 2)
+quantidade = numeros.count(2)
+print(quantidade)
